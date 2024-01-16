84b263029c38351bc80997002eb08c5301532edc
MaiyonDevMacOSX <maiyonaisu1102@gmail.com>
2024-01-16 23:18:29 +0700

    Module3InitProject-MaiyonDevMac

---
+++ Lesson7 - SpringSecurity/demo_spring_security/src/main/java/com/ra/service/impl/RoleServiceImpl.java
@@ -0,0 +1,17 @@
+package com.ra.service.impl;
+
+import com.ra.model.entity.Role;
+import com.ra.repository.RoleRepository;
+import com.ra.service.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements IRoleService {
+    @Autowired
+    private RoleRepository roleRepository;
+    @Override
+    public Role findByRoleName(String roleName) {
+        return roleRepository.findByRoleName(roleName).orElseThrow(() -> new RuntimeException("role not found"));
+    }
+}
